30c07f55483fcf43121a35b9b3e9e6f8e786c647
adriandelgg <adriandelgadoc1@gmail.com>
2021-06-02 00:15:33 -0700

    Added withdrawal & deposit functionality along w/ tests

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -4,18 +4,44 @@ import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 import "./ColorMinter.sol";
 
 contract Exchange is Ownable, ColorMinter {
+    event TokenPurchased(uint256 tokenId, address buyer, address seller);
+
     mapping(address => string[]) public soldToken;
+
+    receive() external payable {}
+
+    fallback() external payable {}
 
     function sellToExchange(uint256 _tokenId, uint256 _sellPrice) public {
         transferFrom(msg.sender, address(this), _tokenId);
     }
 
-    function buyToken() public {}
+    function buyToken(uint256 _tokenId) public payable {
+        transferFrom(address(this), msg.sender, _tokenId);
+        emit TokenPurchased(_tokenId, msg.sender, address(this)); // Instead of this it should be the original seller
+    }
+
+    function paySellerAfterPurchase() private {}
+
+    function withdrawAll() public onlyOwner {
+        address payable _owner = payable(owner());
+        (bool success, ) = _owner.call{value: address(this).balance}("");
+        require(success, "Transaction failed.");
+    }
+
+    function giveEther() public payable {
+        // address payable thisContract = payable(this);
+        payable(this).transfer(1 ether);
+        // address payable cont = payable(this);
+        // (bool success, ) = cont.call{value: 1 ether}("");
+        // require(success, "Transaction failed.");
+    }
+
     //1. Give exchange NFT to sell
     // Will change owner ship to contract, but contract will remember who sent it by storing in a mapping
     // When someone buy it, the contract will transfer to them, take 3%, and send the remaining amount to seller
     // emit sell event
-    //2. Owner can withdraw exchanges funds
+    //2. Owner can withdraw exchange's funds
     // function withdraw() onlyOwner {
     // Withdraws to owner's wallet address
     // emit withdraw event
--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -1,24 +1,43 @@
-// const Exchange = artifacts.require('Exchange');
+const Exchange = artifacts.require('Exchange');
+const Web3 = require('web3');
+const web3 = new Web3('HTTP://127.0.0.1:7545');
 
-// contract('Exchange', accounts => {
-// 	const [alice, bob] = accounts;
-// 	let contract;
-// 	beforeEach(async () => {
-// 		contract = await Exchange.new();
-// 	});
+contract('Exchange', accounts => {
+	const [alice, bob] = accounts;
+	let contract;
+	beforeEach(async () => {
+		contract = await Exchange.new();
+	});
 
-// 	xdescribe('Owner', async () => {
-// 		it("should say I'm the owner", async () => {
-// 			const result = await contract.owner();
-// 			assert.equal(result, alice);
-// 		});
+	xdescribe('Owner', async () => {
+		xit("should say I'm the owner", async () => {
+			const result = await contract.owner();
+			assert.equal(result, alice);
+		});
 
-// 		it('should transfer ownership', async () => {
-// 			const result = await contract.transferOwnership(bob);
-// 			const newOwner = await contract.owner();
-// 			assert.equal(newOwner, bob);
-// 		});
+		xit('should transfer ownership', async () => {
+			const result = await contract.transferOwnership(bob);
+			const newOwner = await contract.owner();
+			assert.equal(newOwner, bob);
+		});
+	});
 
-// 		describe('', () => {});
-// 	});
-// });
+	describe('Owner Payable', async () => {
+		it('should withdrawal all ether from contract', async () => {
+			const initialAmount = web3.utils.fromWei(
+				await web3.eth.getBalance(contract.address)
+			);
+
+			const tx = await contract.giveEther({
+				from: alice,
+				value: web3.utils.toWei('1')
+			});
+			console.log(tx);
+			const newAmount = web3.utils.fromWei(
+				await web3.eth.getBalance(contract.address)
+			);
+			console.log(initialAmount, newAmount);
+			assert(initialAmount < newAmount);
+		});
+	});
+});
